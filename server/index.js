727fd1ea9a450a58271b4e1582e358dd4d18e5c0
Arin Tiwari <arintiwari68@gmail.com>
2024-11-01 21:17:25 +0530

    Updated backend URL configuration

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:5173",
+		origin: ["http://localhost:5173", "https://learn-new.vercel.app"],
 		credentials:true,
 	})
 )
